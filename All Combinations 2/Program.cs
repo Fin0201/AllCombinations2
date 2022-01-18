98b233c1e7d1ce249cdfd86dccdc7ddf26a39bf5
Finley Edwards <EDW17006550@btc.ac.uk>
2022-01-18 07:43:08 +0000

    Add project files.

---
+++ All Combinations 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1234;
+
+            string xString = x.ToString(); //Converts the integer to a string so each character can be easily displayed seperately.
+
+
+
+            Console.WriteLine($"{xString[0]}{xString[1]}{xString[2]}{xString[3]}"); //Manually outputs all the combinations.
+            Console.WriteLine($"{xString[0]}{xString[1]}{xString[3]}{xString[2]}");
+            Console.WriteLine($"{xString[0]}{xString[2]}{xString[1]}{xString[3]}");
+            Console.WriteLine($"{xString[0]}{xString[2]}{xString[3]}{xString[1]}");
+            Console.WriteLine($"{xString[0]}{xString[3]}{xString[1]}{xString[2]}");
+            Console.WriteLine($"{xString[0]}{xString[3]}{xString[2]}{xString[1]}");
+
+            Console.WriteLine($"{xString[1]}{xString[1]}{xString[2]}{xString[3]}");
+            Console.WriteLine($"{xString[1]}{xString[1]}{xString[3]}{xString[2]}");
+            Console.WriteLine($"{xString[1]}{xString[2]}{xString[1]}{xString[3]}");
+            Console.WriteLine($"{xString[1]}{xString[2]}{xString[3]}{xString[1]}");
+            Console.WriteLine($"{xString[1]}{xString[3]}{xString[1]}{xString[2]}");
+            Console.WriteLine($"{xString[1]}{xString[3]}{xString[2]}{xString[1]}");
+
+            Console.WriteLine($"{xString[2]}{xString[1]}{xString[2]}{xString[3]}");
+            Console.WriteLine($"{xString[2]}{xString[1]}{xString[3]}{xString[2]}");
+            Console.WriteLine($"{xString[2]}{xString[2]}{xString[1]}{xString[3]}");
+            Console.WriteLine($"{xString[2]}{xString[2]}{xString[3]}{xString[1]}");
+            Console.WriteLine($"{xString[2]}{xString[3]}{xString[1]}{xString[2]}");
+            Console.WriteLine($"{xString[2]}{xString[3]}{xString[2]}{xString[1]}");
+
+            Console.WriteLine($"{xString[2]}{xString[1]}{xString[2]}{xString[3]}");
+            Console.WriteLine($"{xString[2]}{xString[1]}{xString[3]}{xString[2]}");
+            Console.WriteLine($"{xString[2]}{xString[2]}{xString[1]}{xString[3]}");
+            Console.WriteLine($"{xString[2]}{xString[2]}{xString[3]}{xString[1]}");
+            Console.WriteLine($"{xString[2]}{xString[3]}{xString[1]}{xString[2]}");
+            Console.WriteLine($"{xString[2]}{xString[3]}{xString[2]}{xString[1]}");
+        }
+    }
+}
